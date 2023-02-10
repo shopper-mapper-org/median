6a3fc218cbc929996a7cc7cccb88c0f32fa1a13a
MichaelMakinde <michael_makinde@outlook.com>
2023-02-10 17:28:38 -0500

    Co-authored-by: John Mal <johnmal-dev@users.noreply.github.com> Co-authored-by: Graydon James (he/him) <graydonj@users.noreply.github.com>

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import React from 'react';
+import axios from 'axios';
 
 const Form = () => {
 
@@ -18,20 +19,45 @@ const Form = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     setUserSubmit(locationInput);
+    console.log('location input: ', locationInput);
+    console.log('query input: ', queryInput);
+    getData();
   }
+
+  const getData = async () => {
+    // https://www.mapquestapi.com/search/v3/prediction?limit=5&collection=address&location=-79.39767202917781,43.64990390157667&q=32 corbett&key=ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7
+    const res = await axios({
+      url: `https://www.mapquestapi.com/search/v4/place`,
+      params: {
+        key: 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7',
+        pageSize: 20,
+        location: "-79.39767202917781, 43.64990390157667",
+        circle: '-79.39767202917781, 43.64990390157667, 20000',
+        sort: 'relevance',
+        q: 'sushi'
+      }
+    })
+    // const res = await fetch('https://www.mapquestapi.com/search/v4/place?limit=10&location=-79.39767202917781,43.64990390157667&q=32 corbett&key=ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7&sort=distance')
+    // const data = await res.json();
+    console.log(res);
+  }
+
 
   return (
     <>
       <section className="form-container container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="current-location-container">
             <label htmlFor="location"></label>
-            <input type="text" id="location" placeholder="Enter location">
+            <input type="text" id="location" placeholder="Enter location" value={locationInput} onChange={handleLocInput} required>
             </input>
           </div>
           <div className="user-query-container">
             <label htmlFor="query"></label>
-            <input type="text" id="query" placeholder="Enter Search Query"></input>
+            <input type="text" id="query" placeholder="Enter Attraction" value={queryInput} onChange={handleQueryInput} required></input>
+          </div>
+          <div className="button-submit-container">
+            <button>Search</button>
           </div>
         </form>
       </section>
