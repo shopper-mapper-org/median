56e88afd75e1167c0b75ad7b4cfdbb89ffa08178
John Malapit <johncmalapit@gmail.com>
2023-02-13 22:36:02 -0500

    implement middleIcon

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,13 +1,18 @@
-import { Icon } from 'leaflet';
+import { Icon } from "leaflet";
 
 const locationIcon = new Icon({
-  iconUrl: 'https://assets.mapquestapi.com/icon/v2/marker-md.png',
+  iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-md.png",
   iconSize: 25,
 });
 
 const resultIcon = new Icon({
-  iconUrl: 'https://assets.mapquestapi.com/icon/v2/marker-sm-F8E71C-417505.png',
+  iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-sm-F8E71C-417505.png",
   iconSize: 25,
 });
 
-export { locationIcon, resultIcon };
+const middleIcon = new Icon({
+  iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-sm-F8E71C-417505.png",
+  iconSize: 25,
+});
+
+export { locationIcon, resultIcon, middleIcon };
