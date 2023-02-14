9709538474c4806b1a57635821ceac731ff11b0e
John Malapit <johncmalapit@gmail.com>
2023-02-14 12:16:46 -0500

    center of map to update on location change

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { locationIcon, resultIcon, middleIcon } from "../utils/icons";
 import "leaflet/dist/leaflet.css";
-import { fetchResults, fetchRoute } from "../utils/services";
+import { fetchRoute } from "../utils/services";
 import Routing from "./Routing";
+import ChangeView from "./ChangeView";
 
-const Map = ({ userQuery, userCoordinates, results, setResults }) => {
+const Map = ({ userCoordinates, results }) => {
   const [route, setRoute] = useState([]);
   const [showRoute, setShowRoute] = useState(false);
   const [destination, setDestination] = useState(null);
-  
-  // useEffect(() => {
-  //   const getResults = async () => {
-  //     const fetchedResults = await fetchResults(userQuery, userCoordinates);
-  //     setResults(fetchedResults);
-  //   };
-  //   getResults();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [userQuery, userCoordinates]);
 
   const handleDirectionsClick = async (userCoords, resultCoords) => {
     const fetchedRoute = await fetchRoute(userCoords, resultCoords);
@@ -33,7 +25,12 @@ const Map = ({ userQuery, userCoordinates, results, setResults }) => {
   return (
     <div className="map-view">
       <h2>Map</h2>
-      <button className='map-view-button' onClick={handleBackToResultsClick}>Back to results</button>
+      <button
+        className="map-view-button"
+        onClick={handleBackToResultsClick}
+      >
+        Back to results
+      </button>
       <MapContainer
         center={userCoordinates}
         zoom={13}
@@ -84,6 +81,10 @@ const Map = ({ userQuery, userCoordinates, results, setResults }) => {
             </Marker>
           </>
         )}
+        <ChangeView
+          center={userCoordinates}
+          zoom={12}
+        />
       </MapContainer>
     </div>
   );
