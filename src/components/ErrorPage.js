2c024ddeab33894e0019732176f1efdffaab5bbd
MichaelMakinde <michael_makinde@outlook.com>
2023-02-15 16:07:40 -0500

    Tweaked text on error

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -4,8 +4,10 @@ const ErrorPage = () => {
     return(
         <div>
             <h1>404!</h1>
-            <p>It seems that you've tried looking for a page that does not exist. Please return to</p>
-            <p>Click <Link to={`/`}><p>here</p></Link> to return to Median...</p>
+            <p>It seems that you've tried looking for a page that does not exist...</p>
+            <div>
+                <p>Click <Link to={`/`}><p>here</p></Link> to return to Median...</p>
+            </div>
         </div>
     )
 }
