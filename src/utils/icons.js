752c800909cc6790dad747df02f76d3296aeb93d
John Malapit <johncmalapit@gmail.com>
2023-02-15 22:42:22 -0500

    fix icon on-click error

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -2,32 +2,32 @@ import { Icon } from "leaflet";
 
 const locationIcon = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-md.png",
-  iconSize: 25,
+  iconSize: [25, 25],
 });
 
 const resultIcon = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-sm-82A05A-C4E39C.png",
-  iconSize: 25,
+  iconSize: [25, 25],
 });
 
 const middleIcon = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/circle-lg-5BC5FF-M.png",
-  iconSize: 35,
+  iconSize: [35, 35],
 });
 
 const faveIcon = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/circle-lg-5BC5FF-C4E39C-F.png",
-  iconSize: 35,
+  iconSize: [35, 35],
 });
 
-const highlightIcon = new Icon ({
+const highlightIcon = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-sm-FF9D7F-FFFFFF.png",
-  iconSize: 25,
-})
+  iconSize: [25, 25],
+});
 
-const middleHighlight = new Icon ({
+const middleHighlight = new Icon({
   iconUrl: "https://assets.mapquestapi.com/icon/v2/marker-md-5BC5FF-FF9D7F-M.png",
-  iconSize: 35,
-})
+  iconSize: [35, 35],
+});
 
 export { locationIcon, resultIcon, middleIcon, faveIcon, highlightIcon, middleHighlight };
