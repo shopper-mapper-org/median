dea8834f9573151791f0a340bb7afe2571b14cdd
graydon james <graydonj2@gmail.com>
2023-02-17 13:22:51 -0500

    Accessibility to Contact page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import background-image from '../assets/luke-stackpoole-ZRsJmpt9pNI-unsplash.jpg'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <div className="container">
-        <h1>MEDIAN</h1>
+        <Link to="/"><h1>MEDIAN</h1></Link>
         <h2>brought to you by the good folks at <strong>SHOPPER MAPPER</strong></h2>
         <h3>the world's most average places at your fingertips</h3>
       </div>
