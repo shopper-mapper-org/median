312a73bcca8ed0ad748a46bf78c2ff7f79218d66
John Malapit <johncmalapit@gmail.com>
2023-02-21 20:21:38 -0500

    update spacing and footer placement

--- src/components/RouteAnimation.js
+++ src/components/RouteAnimation.js
@@ -1,39 +1,41 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router";
-import { AnimatePresence } from "framer-motion"
+import { AnimatePresence } from "framer-motion";
 import Main from "./Main";
 import About from "./About";
 import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
 
 const RouteAnimation = () => {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    return (
-        <>
-            <AnimatePresence >
-                <Routes location={location} key={location.id}>
-                    <Route
-                        path="/"
-                        element={<Main />}
-                    />
-                    <Route
-                        path="/About"
-                        element={<About />}
-                    />
-                    <Route
-                        path="/Contact"
-                        element={<Contact />}
-                    />
-                    <Route
-                        path="*"
-                        element={<ErrorPage />}
-                    />
-                </Routes>
-            </AnimatePresence>
-        </>
-    )
-}
+  return (
+    <div className="main">
+      <AnimatePresence>
+        <Routes
+          location={location}
+          key={location.id}
+        >
+          <Route
+            path="/"
+            element={<Main />}
+          />
+          <Route
+            path="/About"
+            element={<About />}
+          />
+          <Route
+            path="/Contact"
+            element={<Contact />}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+};
 
 export default RouteAnimation;
