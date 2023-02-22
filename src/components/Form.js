d20069bc9c1da1002cbaf2fbf7a5d8685210a1f7
John Malapit <johncmalapit@gmail.com>
2023-02-21 22:38:00 -0500

    fix eslint errors

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,74 +1,76 @@
-import { useState } from 'react';
-import React from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import React from "react";
+import axios from "axios";
 
 const Form = () => {
-
   const [locationInput, setLocationInput] = useState("");
+  // eslint-disable-next-line no-unused-vars
   const [userSubmit, setUserSubmit] = useState("");
   const [queryInput, setQueryInput] = useState("");
 
   const handleLocInput = (event) => {
     setLocationInput(event.target.value);
-  }
+  };
 
   const handleQueryInput = (event) => {
     setQueryInput(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     setUserSubmit(locationInput);
-    console.log('location input: ', locationInput);
-    console.log('query input: ', queryInput);
+    console.log("location input: ", locationInput);
+    console.log("query input: ", queryInput);
     getData();
-  }
+  };
 
   const getData = async () => {
     // https://www.mapquestapi.com/search/v3/prediction?limit=5&collection=address&location=-79.39767202917781,43.64990390157667&q=32 corbett&key=ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7
     const res = await axios({
       url: `https://www.mapquestapi.com/search/v4/place`,
       params: {
-        key: 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7',
+        key: "ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7",
         pageSize: 20,
         location: "-79.39767202917781, 43.64990390157667",
-        circle: '-79.39767202917781, 43.64990390157667, 20000',
-        sort: 'relevance',
-        q: 'sushi'
-      }
-    })
+        circle: "-79.39767202917781, 43.64990390157667, 20000",
+        sort: "relevance",
+        q: "sushi",
+      },
+    });
     // const res = await fetch('https://www.mapquestapi.com/search/v4/place?limit=10&location=-79.39767202917781,43.64990390157667&q=32 corbett&key=ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7&sort=distance')
     // const data = await res.json();
     console.log(res);
-  }
-
+  };
 
   return (
     <>
       <section className="container">
         {/* Placeholder, change to desired text */}
-        <form className="form-container" onSubmit={handleSubmit}>
+        <form
+          className="form-container"
+          onSubmit={handleSubmit}
+        >
           <div className="location-container">
             <label htmlFor="location">Enter your Location or Starting Point</label>
-            <input 
-              type="text" 
-              id="location" 
-              placeholder="Enter Location" 
-              value={locationInput} 
-              onChange={handleLocInput} 
-              required>
-            </input>
+            <input
+              type="text"
+              id="location"
+              placeholder="Enter Location"
+              value={locationInput}
+              onChange={handleLocInput}
+              required
+            ></input>
           </div>
           <div className="query-container">
             <label htmlFor="query">Try Searching for an Attraction (e.g Museum, Restaurant, etc..)</label>
-            <input 
-              type="text" 
-              id="query" 
-              placeholder="Enter Attraction" 
-              value={queryInput} 
-              onChange={handleQueryInput} 
-              required>
-            </input>
+            <input
+              type="text"
+              id="query"
+              placeholder="Enter Attraction"
+              value={queryInput}
+              onChange={handleQueryInput}
+              required
+            ></input>
           </div>
           <div className="button-container">
             <button>Search</button>
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Results = ({ results, highlight, setHighlight }) => {
   // using index value of the result array to determine highlighted result
@@ -12,12 +12,10 @@ const Results = ({ results, highlight, setHighlight }) => {
 
     // if we have an odd length array...
     if (results.length > 0 && results.length % 2) {
-
       // we have 1 value for the middle
       curHighlightID.push(results[curIndex].id);
       curHighlight.push(results[curIndex]);
     } else if (results.length > 0) {
-
       // otherwise, we have an even length array & we have 2 values for the middle
       curHighlightID.push(results[curIndex].id);
       curHighlightID.push(results[curIndex + 1].id);
@@ -27,11 +25,11 @@ const Results = ({ results, highlight, setHighlight }) => {
 
     setHighlightID(curHighlightID);
     setHighlight(curHighlight);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [results]);
 
   // handle the user making selections in our select box
   const handleSelect = (event) => {
-
     // set up empty array for highlight IDs and highlight results
     const curHighlightID = [];
     const curHighlight = [];
@@ -39,7 +37,6 @@ const Results = ({ results, highlight, setHighlight }) => {
     // loop through selected options
     const curOptions = Array.from(event.target.selectedOptions);
     curOptions.forEach((option) => {
-
       // push the ID into our highlighted IDs array
       curHighlightID.push(option.value);
 
@@ -48,39 +45,44 @@ const Results = ({ results, highlight, setHighlight }) => {
         if (result.id === option.value) {
           curHighlight.push(result);
         }
-      })
-    })
+      });
+    });
 
     // set our state
     setHighlightID(curHighlightID);
     setHighlight(curHighlight);
-  }
+  };
 
   return (
-    <div className="results-view">    
-      <label htmlFor="result-select"><h2>Results</h2></label>
+    <div className="results-view">
+      <label htmlFor="result-select">
+        <h2>Results</h2>
+      </label>
       <div className="results">
-        {(
-          (highlight.length > 0) ? 
-            <select
-              name="results-select"
-              id="results-select"
-              multiple
-              defaultValue={highlightID}
-              size={results.length}
-              onChange={handleSelect}>
-                {results.map((result) => {
-                  return (
-                    <option key={result.id} value={result.id}>
-                      {result.name}, {result.place.properties.street}, {result.place.properties.postalCode}
-                    </option>
-                  )
-                })}
-            </select> : null
-        )}
+        {highlight.length > 0 ? (
+          <select
+            name="results-select"
+            id="results-select"
+            multiple
+            defaultValue={highlightID}
+            size={results.length}
+            onChange={handleSelect}
+          >
+            {results.map((result) => {
+              return (
+                <option
+                  key={result.id}
+                  value={result.id}
+                >
+                  {result.name}, {result.place.properties.street}, {result.place.properties.postalCode}
+                </option>
+              );
+            })}
+          </select>
+        ) : null}
       </div>
     </div>
-  )
+  );
 };
 
 export default Results;
